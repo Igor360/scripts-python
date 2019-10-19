5c1204a16c91cc60b55386d80dae686d7fc220b6
Igor360 <igor.dawidencko@gmail.com>
2019-10-19 18:58:02 +0300

    Initial commit

---
+++ temp-service2.py
@@ -0,0 +1,50 @@
+import time
+import datetime
+import glob
+import MySQLdb
+import Adafruit_DHT
+from time import strftime
+ 
+
+code = 'dth11-temp'
+
+#Variables for sensor
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 26
+
+# Variables for MySQL
+db = MySQLdb.connect(host="localhost", user="root",passwd="password", db="HN_database")
+cur = db.cursor()
+ 
+def tempRead():
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if temperature is not None and humidity is not None:
+        return [round(temperature, 1), round(humidity, 1)]
+    else:
+        return None
+ 
+
+while True:    
+    temp, humidity = tempRead()
+    if temp is None:
+        continue
+    datetimeWrite = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
+    print datetimeWrite
+    sql = ("""INSERT INTO sensor_data (created_at,value, name, code) VALUES (%s, %s, %s, %s)""",(datetimeWrite, temp, 'temperature', code))
+    sql2 = ("""INSERT INTO sensor_data (created_at,value, name, code) VALUES (%s, %s, %s, %s)""",(datetimeWrite, humidity, 'hudaminity', code))
+    try:
+        print "Writing to database..."
+        # Execute the SQL command
+        cur.execute(*sql)
+        cur.execute(*sql2)
+        db.commit()
+        print "Write Complete"
+
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        print "Failed writing to database"
+ 
+    cur.close()
+    db.close()
+    break
